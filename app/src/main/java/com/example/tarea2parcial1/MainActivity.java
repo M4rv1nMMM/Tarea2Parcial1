985263b351752fad1e5de3d4be3e69eec0d5ada0
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-06-23 01:38:57 -0600

    Programado el Primer Acitivity de envio de datos

--- app/src/main/java/com/example/tarea2parcial1/MainActivity.java
+++ app/src/main/java/com/example/tarea2parcial1/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.tarea2parcial1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etxtNombre;
+    private EditText etxtApellidos;
+    private EditText etxtEdad;
+    private EditText etxtCorreo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etxtNombre = (EditText) findViewById(R.id.txtNombre);
+        etxtApellidos = (EditText) findViewById(R.id.txtApellidos);
+        etxtEdad = (EditText) findViewById(R.id.txtEdad);
+        etxtCorreo = (EditText) findViewById(R.id.txtCorreo);
+    }
+
+    public void Enviar (View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        i.putExtra("name", etxtNombre.getText().toString());
+        i.putExtra("name2", etxtApellidos.getText().toString());
+        i.putExtra("age", etxtEdad.getText().toString());
+        i.putExtra("mail", etxtCorreo.getText().toString());
+        startActivity(i);
     }
 }
