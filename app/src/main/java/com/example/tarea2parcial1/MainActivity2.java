d9f0841a936728b1d3f89ae34fe6437571bd4b2b
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-06-23 02:09:42 -0600

    Programado el Segundo Acitivity de recibo de datos

--- app/src/main/java/com/example/tarea2parcial1/MainActivity2.java
+++ app/src/main/java/com/example/tarea2parcial1/MainActivity2.java
@@ -2,13 +2,41 @@ package com.example.tarea2parcial1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView ttxtnombre2;
+    private TextView ttxtapellidos2;
+    private TextView ttxtedad2;
+    private TextView ttxtcorreo2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ttxtnombre2 = (TextView) findViewById(R.id.txtNombre2);
+        ttxtapellidos2 = (TextView) findViewById(R.id.txtApellidos2);
+        ttxtedad2 = (TextView) findViewById(R.id.txtEdad2);
+        ttxtcorreo2 = (TextView) findViewById(R.id.txtCorreo2);
+
+        String varname = getIntent().getStringExtra("name");
+        String varname2 = getIntent().getStringExtra("name2");
+        String varage = getIntent().getStringExtra("age");
+        String varmail = getIntent().getStringExtra("mail");
+
+        ttxtnombre2.setText(varname);
+        ttxtapellidos2.setText(varname2);
+        ttxtedad2.setText(varage);
+        ttxtcorreo2.setText(varmail);
+    }
+
+    public void regresar(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
